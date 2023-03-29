fc91f05139b6b9affdbd6b4062842c2c518507ba
rosercode <rosercode@gmail.com>
2023-03-29 19:44:31 +0800

    refactor Improve "port-killer" script with comments, functions
    - Add comments for better code readability and maintainability
    - (main) Refactor code into functions for better organization and reuse
    - Change exit codes to indicate error conditions
    - Update error messages for better clarity and consistency

--- port-killer.sh
+++ port-killer.sh
@@ -5,35 +5,52 @@
 # 创建日期：2023/03/29 05:54
 # 作者：rosercode@gmail.com
 
+# 检查参数
 if [ -z "$1" ]; then
     echo "请输入参数"
-    exit 0
+    exit 1
 fi
 
+# 检查端口号
+function check_port {
+    if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+        echo "端口号必须是一个数字"
+        exit 1
+    fi
 
-# 判断
-if ! [[ "$1" =~ ^[0-9]+$ ]]; then
-    echo "你输入的不是一个数字"
-    exit 0
-fi
+    if ! [ "$1" -ge 1 -a "$1" -le 65535 ]; then
+        echo "端口号必须在 1-65535 范围内"
+        exit 1
+    fi
+}
 
+# 获取进程 PID
+function get_process_pid {
+    local pid=`lsof -i:$1 -t`
+    echo $pid
+}
 
-if ! [ "$1" -ge 1 -a "$1" -le 65535 ]; then
-    echo "请输入一个合适的端口 1-65535 的范围内。"
-    exit 0
-fi
-
-process_pid=`lsof -i:$1 -t`
-
-if [[ "$process_pid" == $'' ]]; then
-    echo "该端口没有进程存在"
-else
-    kill -9 $process_pid
+# 杀死进程
+function kill_process {
+    kill -9 $1
     if [ $? -eq 0 ]; then
-        echo "该进程已经终止"
+        echo "进程 $1 已经终止"
     else
         echo "命令执行失败。"
+        exit 1
     fi
-fi
+}
 
-exit 0
+# 主函数
+function main {
+    check_port $1
+    process_pid=`get_process_pid $1`
+    if [[ "$process_pid" == $'' ]]; then
+        echo "该端口没有进程存在"
+    else
+        kill_process $process_pid
+    fi
+    exit 0
+}
+
+main $1
